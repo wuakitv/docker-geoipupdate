a75b7dcf7b519dc233f9216f901b43fd6d65f8a4
Leonardo Damasceno <leonardo.damasceno@wsp11198-3.local>
2019-05-16 11:24:09 +0200

    Updated Dockerfile to add bash

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM golang:alpine as build
 
 ENV VERSION=4.0.2
+VOLUME /geodata
 RUN apk add --update wget git bash
 RUN wget -P /tmp/build/ https://github.com/maxmind/geoipupdate/archive/v${VERSION}.tar.gz
 RUN tar -C /tmp/build/ -zxvf /tmp/build/v${VERSION}.tar.gz
@@ -14,9 +15,10 @@ RUN go build
 
 RUN cp /tmp/build/geoipupdate-${VERSION}/cmd/geoipupdate/geoipupdate /usr/bin/
 
-FROM alpine:3.8
+FROM alpine
+
+RUN apk add -q --update bash
 
 COPY --from=build /usr/bin/geoipupdate /usr/bin/
-
 COPY update.sh /usr/bin/
 ENTRYPOINT ["update.sh"]
--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# update.sh downloads the latests updates from MaxMind GeoIP service as 
+# update.sh downloads the latests updates from MaxMind GeoIP service as
 # specified by a given conf file.
 # @see: http://dev.maxmind.com/geoip/geoipupdate/
 
 conf=$@
 geoipupdate -d /geodata -f ${conf} 2 > /dev/null
 
-# The geoipupdate binary doesn't like it when the mmdb file does not already 
+# The geoipupdate binary doesn't like it when the mmdb file does not already
 # exist; it will write to stderr instead and not removing the intermediatary
 # gz file. This if statement here naively tries to run it again to let the
 # executable update itself.
